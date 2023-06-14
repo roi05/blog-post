e30761487d5bda62d6881972663af40f441af330
Roi Buenaventura <roibuenaventura05@gmail.com>
2023-06-14 13:50:02 +0800

    fix an issue

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { execSync } = require('child_process');
+const nextConfig = {}
 
-const nextConfig = {
-  // ...existing Next.js configuration...
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      execSync('npx prisma generate');
-    }
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
